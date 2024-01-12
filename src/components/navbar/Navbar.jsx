3c512c7071161c3ad5a4898cc6534933299e8f3e
Lakshya Kumar <kumarlakshya101@gmail.com>
2024-01-12 23:10:41 +0530

    formatted navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
-import {useState, useEffect} from "react";
-import {navbarData} from "../../dataForPage/DummyData";
+import { useState, useEffect } from "react";
+import { navbarData } from "../../dataForPage/DummyData";
 import logo from "../../assets/images/logo.svg";
 import { Link } from "react-scroll";
 import { navbar } from "@material-tailwind/react";
@@ -12,21 +12,32 @@ const Navbar = () => {
           <div>
             <img className="h-10" src={logo} alt="logo-img"></img>
           </div>
-          <div className='flex flex-row items-center w-full'>{navbarData.map((item) => {
-            return (
-              <div key = {item.id}>
-                <Link to={item.link} spy={true} smooth={true} hashSpy={true} offset={50} duration={500} isDynamic={true}
-                ignoreCancelEvents={false} spyThrottle={500} className='cursor-pointer text-dark font-inter text-lg font-medium tracking-tight py-1 px-2 hover:text-blue-500'
-                >
-                  {item.name}
-                </Link>
-              </div>
-            )
-          })}</div>
+          <div className="flex flex-row items-center w-full">
+            {navbarData.map((item) => {
+              return (
+                <div key={item.id}>
+                  <Link
+                    to={item.link}
+                    spy={true}
+                    smooth={true}
+                    hashSpy={true}
+                    offset={50}
+                    duration={500}
+                    isDynamic={true}
+                    ignoreCancelEvents={false}
+                    spyThrottle={500}
+                    className="cursor-pointer text-dark font-inter text-lg font-medium tracking-tight py-1 px-2 hover:text-blue-500"
+                  >
+                    {item.name}
+                  </Link>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Navbar;
