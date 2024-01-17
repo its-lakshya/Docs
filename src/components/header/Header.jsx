f640bf913ea731b7ce113b2fbf47f3d96c2e06ad
Lakshya Kumar <kumarlakshya101@gmail.com>
2024-01-17 23:51:20 +0530

    formatted the code

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,24 @@
+import React from "react";
 import { Switch } from "@material-tailwind/react";
 
 const Header = () => {
-    return (
-        <div className='mx-auto pt-16'>
-            <div className='grid grid-col justify-items-center items-center'>
-                <div className='w-4/5 pl-24'>
-                    <Switch defaultChecked/>
-                    <h1 className='text-5xl font-russonOne font-medium no-underline align-middle tracking-wide normal-case leading-normal text-dark'>
-                        Axel Brand new 
-                        <span className ='text-dark font-petitFormal font-bold'> Furniture</span>{" "}
-                        Mobile App
-                    </h1>
-                </div>
-            </div>
+  return (
+    <div className="mx-auto pt-16">
+      <div className="grid grid-col justify-items-center items-center">
+        <div className="w-4/5 pl-24">
+          <Switch defaultChecked />
+          <h1 className="text-5xl font-russonOne font-medium no-underline align-middle tracking-wide normal-case leading-normal text-dark">
+            Axel Brand new{" "}
+            <span className="text-dark font-petitFormal font-bold">
+              {" "}
+              Furniture
+            </span>{" "}
+            Mobile App
+          </h1>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Header;
