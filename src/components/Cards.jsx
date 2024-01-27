2a23e568a289f62a2530549ebd6bc61568c9a6be
Lakshya Kumar <kumarlakshya101@gmail.com>
2024-01-27 23:50:59 +0530

    made the card component and made it dragable using framer motion

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { FaRegFileAlt } from "react-icons/fa";
+import { FiDownload } from "react-icons/fi";
+import { IoCloseSharp } from "react-icons/io5";
+import {motion} from "framer-motion"
 
-const Cards = () => {
+
+const Cards = ({data, reference}) => {
   return (
-    <div className='w-60 h-72 rounded-[20px] bg-black/50'></div>
+    <motion.div drag dragConstraints={reference} whileDrag={{scale : 1.2}} dragElastic={0.1} className='relative w-60 h-72 rounded-[45px] bg-black/60 text-white px-8 py-10 overflow-hidden'>
+      <FaRegFileAlt/>
+      <p className='text-sm leading-tight mt-5 font-semibold'>{data.desc}</p>
+      <div className='absolute bottom-0 left-0 w-full'>
+        <div className='flex items-center justify-between mb-3 py-3 px-8'>
+          <h5>{data.fileSize}</h5>
+          <span className='w-5 h-5 bg-light-blue-200 rounded-full flex items-center justify-center'>
+            {data.close ? <IoCloseSharp/> : <FiDownload/>}
+          </span>
+        </div>
+        {data.tag.isOpen ? (
+          <div className={`w-full py-4 ${data.tag.tagColor === "blue" ? 'bg-light-blue-200' : "bg-green-500"} flex items-center justify-center`}>
+          <h3 className='text-md'>{data.tag.tagTitle} </h3>
+        </div>
+        ) : null}
+        
+      </div>
+    </motion.div>
   )
 }
 
--- src/components/Forground.jsx
+++ src/components/Forground.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useRef } from "react";
 import Cards from "./Cards";
 
+
 function Forground() {
+  const ref = useRef(null);
+  const data = [
+    {
+      desc : "lorem7 impsum dolor sit amet consecteture adipisicing.",
+      fileSize : ".9mb",
+      close : false,
+      tag : {isOpen : true, tagTitle : "Download Now", tagColor : "green"},
+    },
+    {
+      desc : "lorem7 impsum dolor sit amet consecteture adipisicing.",
+      fileSize : ".9mb",
+      close : true,
+      tag : {isOpen : false, tagTitle : "Download Now", tagColor : "blue"},
+    },
+    {
+      desc : "lorem7 impsum dolor sit amet consecteture adipisicing.",
+      fileSize : ".9mb",
+      close : true,
+      tag : {isOpen : true, tagTitle : "Download Now", tagColor : "blue"},
+    }
+  ]
+
   return (
-    <div className="fixed w-full h-full top-0 left-0 z-[3]">
-      <Cards />
+    <div ref={ref} className="fixed w-full h-full top-0 left-0 z-[3] flex gap-5 flex-wrap ">
+      {data.map((item, index) => {
+        return(
+          <Cards data={item} reference={ref}/>
+        )
+      })}
     </div>
   );
 }
